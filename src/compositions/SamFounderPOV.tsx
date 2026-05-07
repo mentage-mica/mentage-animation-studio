df5eb66c710ededada8d0716488f6ed5048047db
mentage-mica <mentagemica@gmail.com>
2026-05-07 16:34:10 -0400

    Create SamFounderPOV component for video series
    This component displays a video series introduction with a title and description.

---
+++ src/compositions/SamFounderPOV.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { AbsoluteFill } from 'remotion';
+import { mentageTheme } from '../theme/mentageTheme';
+
+  export const SamFounderPOV: React.FC = () => {
+  return (
+    <AbsoluteFill style={{ backgroundColor: mentageTheme.colors.offWhite }}>
+      <div style={{ padding: 60, color: mentageTheme.colors.darkCharcoal }}>
+        <h1 style={{ fontSize: 72, marginBottom: 40 }}>Founder POV</h1>
+        <p style={{ fontSize: 20, maxWidth: 800 }}>
+          A video series exploring insights from founders and mentors.
+        </p>
+      </div>
+    </AbsoluteFill>
+  );
+};
