6a695b1529b71c12f9df9afbe8be281e9f8ea3d5
mentage-mica <mentagemica@gmail.com>
2026-05-07 16:29:46 -0400

    Add RemotionRoot component with two compositions

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { Composition } from 'remotion';
+import { SamWalkingDemo } from './compositions/SamWalkingDemo';
+import { SamFounderPOV } from './compositions/SamFounderPOV';
+
+export const RemotionRoot = () => {
+  return (
+      <>
+            <Composition
+                    id="SamWalkingDemo"
+                            component={SamWalkingDemo}
+                                    durationInFrames={360}
+                                            fps={30}
+                                                    width={1920}
+                                                            height={1080}
+                                                                  />
+                                                                        <Composition
+                                                                                id="SamFounderPOV"
+                                                                                        component={SamFounderPOV}
+                                                                                                durationInFrames={2700}
+                                                                                                        fps={30}
+                                                                                                                width={1920}
+                                                                                                                        height={1080}
+                                                                                                                              />
+                                                                                                                                  </>
+                                                                                                                                    );
+                                                                                                                                    };
+                                                                                                                                    
